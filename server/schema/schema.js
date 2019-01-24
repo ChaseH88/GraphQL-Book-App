3e0f4a281ef2f0f30e5a812eed9c4c2a54b06e49
ChaseH88 <ChaseH88@gmail.com>
2019-01-24 13:24:56 -0600

    Added Add Author Mutation

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -133,6 +133,20 @@ const Mutation = new GraphQLObjectType({
                 });
                 return book.save();
             }
+        },
+        addAuthor: {
+            type: AuthorType,
+            args: {
+                name: {type: new GraphQLNonNull(GraphQLString)},
+                age: {type: new GraphQLNonNull(GraphQLInt)}
+            },
+            resolve(parent, args){
+                let author = new Author({
+                    name: args.name,
+                    age: args.age
+                });
+                return author.save();
+            }
         }
     }
 })
